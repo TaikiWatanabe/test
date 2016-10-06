c763554f1ab4393b8d2781e45570446f67243c65
渡辺泰伎 <b1014147@fun.ac.jp>
2016-10-06 17:12:47 +0900

    [add]ファイル

---
+++ ytaodlsjhf/ytaodlsjhf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ytaodlsjhf
+//
+//  Created by 渡辺泰伎 on 2016/10/06.
+//  Copyright © 2016年 Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
