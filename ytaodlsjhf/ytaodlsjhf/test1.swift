374e46bd884fa9f609db4821417a33536e782dd1
渡辺泰伎 <b1014147@fun.ac.jp>
2016-10-06 17:25:23 +0900

    [add]

---
+++ ytaodlsjhf/ytaodlsjhf/test1.swift
@@ -0,0 +1,25 @@
+//
+//  test1.swift
+//  ytaodlsjhf
+//
+//  Created by 渡辺泰伎 on 2016/10/06.
+//  Copyright © 2016年 Project. All rights reserved.
+//
+
+import UIKit
+
+class test1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
